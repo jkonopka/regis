adcb36fe9c38af57467175d67f4a60fcf8a8f63c
Alexander Staubo <alex@bengler.no>
2014-03-11 23:44:59 -0400

    Just halt with 503 if rate-limited.

--- api/v1/geocode.rb
+++ api/v1/geocode.rb
@@ -1,22 +1,13 @@
-# encoding: utf-8
-
 module Regis
-
   class V1 < Sinatra::Base
 
     post '/geocode' do
       search_address = params[:address]
       config = params[:config] || {}
       @data = Regis.search(search_address, config)
-      # TODO when regis.search fails hard, @data comes back as an empty array... must fix that
-      if(@data.rate_limited)
-        response.status = 503
-      end
+      halt 503, "Rate-limited" if @data.rate_limited
       @data.to_json
     end
 
   end
-
-
-
 end
